0675d6f9ad97745547c57ba11bc0d348a3504726
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2025-10-04 13:32:15 +0700

    create project

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Smart Calo – Student Project</h1>
+      <p>
+        This is a mobile application developed by students at FPT University (Ho
+        Chi Minh City campus) as part of the EXE201 course.
+      </p>
+      <p>
+        Our app helps users track calories, exercise, and meal plans to improve
+        nutrition and health.
+      </p>
+      <p>Team name: Penta</p>
+      <p>Contact: +84 977 064 053</p>
+      <h5>
+        GitHub:{" "}
+        <a href="https://github.com/Sang1011/smartCaloFE">smartCaloFE</a>
+      </h5>
+    </>
+  );
+}
+
+export default App;
